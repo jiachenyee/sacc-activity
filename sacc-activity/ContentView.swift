63defa69f1dc65f4962b34c2c6f256de15220a70
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-11-13 13:08:43 +0800

    Initial Commit

---
+++ sacc-activity/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sacc-activity
+//
+//  Created by Jia Chen Yee on 13/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sacc-activity/sacc_activityApp.swift
@@ -0,0 +1,17 @@
+//
+//  sacc_activityApp.swift
+//  sacc-activity
+//
+//  Created by Jia Chen Yee on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct sacc_activityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
